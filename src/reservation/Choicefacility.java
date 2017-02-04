35ca3d05751abc196ac8fd1b68782c82146751f7
Yuusei-FIRE <yu3spk_sm@docomo.ne.jp>
2017-02-05 02:17:44 +0900

    20170130 submit

---
+++ src/reservation/Choicefacility.java
@@ -0,0 +1,21 @@
+package reservation;
+
+import java.awt.Choice;
+
+public class Choicefacility extends Choice {
+	
+public Choicefacility(){
+	add("小ホール ");
+	add(" 大会議室1");
+	add(" 大会議室2");
+	add(" 小会議室1");
+	add(" 小会議室2");
+	add(" 小会議室3");
+	add(" 小会議室4");
+	add(" 小会議室5");
+	add(" 小会議室6");
+	}
+
+
+}
+
